bab909137cdb3900289f72b0dd8d4df647da117e
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-10 17:39:32 +0100

    feat: Add sub string logic

--- sub-strings/sub_strings.rb
+++ sub-strings/sub_strings.rb
@@ -1,1 +1,16 @@
-dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+def substrings(str, dict_arr)
+  count_word = Hash.new(0)
+  str = str.downcase
+
+  dict_arr.each do |dict_word|
+    index = 0
+    while (index = str.index(dict_word, index))
+      count_word[dict_word] += 1
+      index += 1
+    end
+  end
+  count_word
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit].freeze
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
