dbfdb97afa3e1d9a4dcae4a378e4c903107c480f
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-10 19:41:32 +0100

    refactor: Add additional logic to bubble sort
    Add flag to break the checking early and update the inner loop to reduce
    check range.

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 def bubble_sort(arr)
-  (arr.length - 1).times do
-    arr.each_with_index do |num, i|
-      if i != arr.length - 1 && num > arr[i + 1]
-        arr[i] = arr[i + 1]
-        arr[i + 1] = num
+  n = arr.length
+
+  (0...n - 1).each do |pass|
+    swapped = false
+
+    (0...n - 1 - pass).each do |i|
+      if arr[i] > arr [i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
       end
     end
+    break unless swapped
   end
-
   arr
 end
 
-to_sort = [4, 3, 78, 2, 0, 2]
-p bubble_sort(to_sort)
+# to_sort = [4, 3, 78, 2, 0, 2]
+# p bubble_sort(to_sort)
