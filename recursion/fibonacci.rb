3866e4bf12596f11ccc1f5bc2d18374fd4b7196b
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-24 16:00:13 +0100

    chore: Prep env for fibonacci.rb

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -1,1 +1,34 @@
 # frozen_string_literal: true
+
+# Fibonacci module
+module Fibonacci
+  class << self
+    # Fibonacci Iterative ver.
+    # @param [Integer] length of the fibonacci sequence
+    # @return [Array: Integer] fibonacci sequence array
+    # @version 1.0.0
+    def fibs(num)
+      puts "Fibonacci iterative ver. Expected outcome: Fibs: #{num} = [0, 1, 1, 2, 3, 5, 8, 13]"
+    end
+
+    # Fibonacci Recursive ver.
+    # @param [Integer] length of the fibonacci sequence
+    # @return [Array: Integer] fibonacci sequence array
+    # @version 1.0.0
+    def fibs_rec(num)
+      puts "Fibonacci recursive ver. Expected outcome: Fibs: #{num} = [0, 1, 1, 2, 3, 5, 8, 13]"
+    end
+  end
+end
+
+# Alias for Fibonacci module
+F = Fibonacci
+
+# Debug
+def debug(num, ver: 1)
+  seq = ver == 1 ? F.fibs(num) : F.fibs_rec(num)
+  seq.inspect
+end
+
+debug(8)
+debug(8, ver: 2)
