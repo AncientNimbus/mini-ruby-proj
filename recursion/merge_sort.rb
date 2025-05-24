91f28f58c986bb8a230d1fc117ee02af4ecaaf7f
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-24 19:56:23 +0100

    feat: Add merge_sort method

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -5,12 +5,30 @@ require_relative 'fibonacci'
 # Merge sort module
 module MergeSort
   class << self
-    # Return a sorted array in descending order
+    # Return a sorted array in ascending order
     # @param arr [Array<Integer>] unsorted array
-    # @return [Array<Integer>] sorted array in descending order
+    # @return [Array<Integer>] sorted array in ascending order
     # @version 1.0.0
     def merge_sort(arr)
-      arr
+      return arr unless arr.size > 1
+
+      left_arr = arr.shift(arr.size / 2)
+      left_sorted = merge_sort(left_arr)
+      right_sorted = merge_sort(arr)
+
+      merge(left_sorted, right_sorted)
+    end
+
+    # Helper method
+    # @param left [Array<Integer>]
+    # @param right [Array<Integer>]
+    # @version 1.1.0
+    def merge(left, right)
+      result = []
+
+      result << (left[0] < right[0] ? left.shift : right.shift) while left.any? && right.any?
+
+      result + left + right
     end
   end
 end
@@ -19,12 +37,12 @@ end
 MS = MergeSort
 
 # Debug
-def fibs(num, ver: 2)
-  ver == 1 ? F.fibs(num) : F.fibs_rec(num)
-end
+# def fibs(num, ver: 2)
+#   ver == 1 ? F.fibs(num) : F.fibs_rec(num)
+# end
 
-unsorted_arr = fibs(8).shuffle
-p "Unsorted: #{unsorted_arr}"
+# unsorted_arr = fibs(8).shuffle
+# p "Unsorted: #{unsorted_arr}"
 
-sorted_arr = MS.merge_sort(unsorted_arr)
-p "Sorted: #{sorted_arr}"
+# sorted_arr = MS.merge_sort(unsorted_arr)
+# p "Sorted: #{sorted_arr}"
