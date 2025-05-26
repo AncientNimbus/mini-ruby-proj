3b19fe94e3f4be70b03a29e008617659a6512c8b
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-26 14:59:27 +0100

    chore: Setup files for linked list project

---
+++ linked_lists/linked_list.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Linked List Class
+class LinkedList
+  # @todo #append(value)
+  # @todo #prepend(value)
+  # @todo #size
+  # @todo #head
+  # @todo #tail
+  # @todo #at(index)
+  # @todo #pop
+  # @todo #contains?(value)
+  # @todo #find(value)
+  # @todo #to_s
+  # @todo #insert_at(value, index)
+  # @todo #remove_at(index)
+end
---
+++ linked_lists/node.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Node Class
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
+end
