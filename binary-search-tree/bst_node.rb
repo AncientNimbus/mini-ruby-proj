86cd79039c98d4dc78501067b87acaf7c6c10806
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-28 11:28:59 +0100

    feat: Add bst_node class

--- binary-search-tree/bst_node.rb
+++ binary-search-tree/bst_node.rb
@@ -1,1 +1,30 @@
 # frozen_string_literal: true
+
+# Binary Search Tree Module
+module BST
+  # BST Node Class
+  # @since 1.0.0
+  # @version 1.0.0
+  class Node
+    include Comparable
+
+    attr_accessor :value, :left, :right
+
+    def initialize(value = nil, left = nil, right = nil)
+      @value = value
+      @left = left
+      @right = right
+    end
+
+    # Allow Comparable features between Node Objects
+    # @param other [BST::Node] another BST Node object
+    # @since 1.0.0
+    # @version 1.0.0
+    def <=>(other)
+      raise TypeError, 'Expecting a Node object' unless other.is_a?(Node)
+      raise StandardError, 'Object value cannot be nil' if other.value.nil?
+
+      value <=> other.value
+    end
+  end
+end
--- binary-search-tree/main.rb
+++ binary-search-tree/main.rb
@@ -1,1 +1,12 @@
 # frozen_string_literal: true
+
+require_relative 'bst_node'
+
+# Test BST::Node
+leave1 = TNode.new(100)
+leave2 = TNode.new(200)
+leave3 = TNode.new(300)
+
+p leave1 < leave2
+p leave2 < leave3
+p leave3 > leave1
