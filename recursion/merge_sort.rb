5727cc4433e323703b3fc4e48fe665dbb9602b3f
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-28 11:58:28 +0100

    chore: Cleanup unwanted comments in MergeSort

--- recursion/merge_sort.rb
+++ recursion/merge_sort.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'fibonacci'
-
 # Merge sort module
 module MergeSort
   class << self
@@ -35,14 +33,3 @@ end
 
 # Alias for Merge sort
 MS = MergeSort
-
-# Debug
-# def fibs(num, ver: 2)
-#   ver == 1 ? F.fibs(num) : F.fibs_rec(num)
-# end
-
-# unsorted_arr = fibs(8).shuffle
-# p "Unsorted: #{unsorted_arr}"
-
-# sorted_arr = MS.merge_sort(unsorted_arr)
-# p "Sorted: #{sorted_arr}"
