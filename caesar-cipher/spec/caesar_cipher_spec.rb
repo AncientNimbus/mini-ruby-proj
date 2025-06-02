2eeaef0b8f85e9736ed5c3e97be7ffa938bea677
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-06-02 15:32:08 +0100

    test: add tests for #shift_character

--- caesar-cipher/spec/caesar_cipher_spec.rb
+++ caesar-cipher/spec/caesar_cipher_spec.rb
@@ -22,10 +22,33 @@ describe CaesarCipher do
   end
 
   describe '#shift_character' do
-    # query method
     context 'with uppercase letter' do
-      it 'converts to lowercase and applies 0 shift factor' do
+      it 'converts to lowercase with shift factor sets to 0' do
         expect(caesar_cipher.shift_character('A', 0)).to eq('a')
+      end
+
+      it 'converts to lowercase with shift factor sets to 25' do
+        expect(caesar_cipher.shift_character('A', 25)).to eq('z')
+      end
+    end
+
+    context 'with lowercase letter' do
+      it 'remains as lowercase with shift factor sets to 0' do
+        expect(caesar_cipher.shift_character('a', 0)).to eq('a')
+      end
+
+      it 'remains as lowercase with shift factor sets to 25' do
+        expect(caesar_cipher.shift_character('a', 25)).to eq('z')
+      end
+    end
+
+    context 'with large shift factor' do
+      it 'wraps around correctly when alphabets set has 26 characters' do
+        expect(caesar_cipher.shift_character('a', 26)).to eq('a')
+      end
+
+      it 'wraps around correctly when shift factor is larger than alphabets set' do
+        expect(caesar_cipher.shift_character('a', 105)).to eq('b')
       end
     end
   end
